6689ba2c4b90cbe3110c65f749cb9d659dfc0fbe
7Roshani <74301621+7Roshani@users.noreply.github.com>
2021-02-15 10:38:14 +0530

    6
    BJP

---
+++ bike8.java
@@ -0,0 +1,17 @@
+lass bike8
+{
+final int speedlimit;
+bike8()
+{
+speedlimit=20;
+}
+void run()
+{
+System.out.println(speedlimit);
+}
+public static void main(String[] args)
+{
+bike8 obj=new bike8();
+obj.run();
+}
+}
